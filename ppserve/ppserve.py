46f135132bbd9fc18d62802066d48ec0e79d5434
Franz Berger <fberger.work@gmail.com>
2018-12-06 13:06:30 +0100

    moved imports and loading of my_bonds.yaml into main()

--- ppserve/ppserve.py
+++ ppserve/ppserve.py
@@ -1,25 +1,5 @@
 #!/usr/bin/env python3
 
-import argparse
-parser = argparse.ArgumentParser(description="Serve quotes and historic prices for use with PortfolioPerformance")
-parser.add_argument(
-    '-c-', '--default-currency', dest='default_currency', default='EUR',
-    help='Default currency used for conversion.'
-)
-parser.add_argument(
-    '--hostname', dest='hostname', default='localhost',
-    help='Host name or address to bind to.'
-)
-parser.add_argument(
-    '--port', dest='port', type=int, default=9444,
-    help='Port number to use.'
-)
-parser.add_argument(
-    '--log-level', dest='log_level', choices=['CRITICAL', 'ERROR', 'WARNING', 'INFO', 'DEBUG'], default='INFO',
-    help='Log verbosity.'
-)
-args = parser.parse_args()
-
 from datetime import date, timedelta
 from time import sleep
 from pathlib import Path
@@ -30,15 +10,6 @@ from threading import Thread
 
 from .price_sources import ArivaPriceSource
 from .price_sources.securities import Bond
-
-import logging
-import coloredlogs
-# logging.basicConfig(format='%(levelname)s: %(name)s: %(message)s', level=getattr(logging, args.log_level))
-logger = logging.getLogger('Main')
-coloredlogs.install(level=getattr(logging, args.log_level))
-
-logger.info('Loading manually configured securities.')
-from .my_bonds import my_bonds
 
 template_path = Path(__file__).parent/"templates"
 portfolio_performance_template = SimpleTemplate(
@@ -130,6 +101,35 @@ def update_sec_prices(sec):
         sec.write_quotes()
 
 def main():
+    import argparse
+    parser = argparse.ArgumentParser(description="Serve quotes and historic prices for use with PortfolioPerformance")
+    parser.add_argument(
+        '-c-', '--default-currency', dest='default_currency', default='EUR',
+        help='Default currency used for conversion.'
+    )
+    parser.add_argument(
+        '--hostname', dest='hostname', default='localhost',
+        help='Host name or address to bind to.'
+    )
+    parser.add_argument(
+        '--port', dest='port', type=int, default=9444,
+        help='Port number to use.'
+    )
+    parser.add_argument(
+        '--log-level', dest='log_level', choices=['CRITICAL', 'ERROR', 'WARNING', 'INFO', 'DEBUG'], default='INFO',
+        help='Log verbosity.'
+    )
+    args = parser.parse_args()
+
+    import logging
+    import coloredlogs
+    # logging.basicConfig(format='%(levelname)s: %(name)s: %(message)s', level=getattr(logging, args.log_level))
+    logger = logging.getLogger('Main')
+    coloredlogs.install(level=getattr(logging, args.log_level))
+
+    logger.info('Loading manually configured securities.')
+    from .my_bonds import my_bonds
+
     try:
         Thread(
             target=run, name="price server",
